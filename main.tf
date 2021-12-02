e42ec23f4da9b4f07e31b866958bd82131a1ec85
Michel Baes <michelbaes@localhost.localdomain>
2021-12-02 17:35:02 +0100

    argo-cd setup completed

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "kubernetes" {
+  # load_config_file       = false
+  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+  host                   = var.kubernetes_cluster_endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws-iam-authenticator"
+    args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
+  }
+}
+
+# helm is a kind of apt-get of kubernetes. 
+# We just need to give him the kubernetes connection parameters
+provider "helm" {
+  kubernetes {
+    # load_config_file       = false
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+    host                   = var.kubernetes_cluster_endpoint
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      command     = "aws-iam-authenticator"
+      args        = ["token", "-i", "${var.kubernetes_cluster_name}"]
+    }
+  }
+}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "argo"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "microservices"
+  chart      = "argo-cd"
+  repository = "https:/argoproj.github.io/argo-helm"
+  namespace  = "argo"
+}
+
